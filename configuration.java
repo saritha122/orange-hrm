87a5c9761c94abd0faa4da5060756977c555d6e0
saritha122 <70686335+saritha122@users.noreply.github.com>
2023-08-25 16:28:12 +0530

    configuration.java

---
+++ configuration.java
@@ -0,0 +1,23 @@
+webdriver.chrome.driver=C:\\Users\\Admin\\Downloads\\chromedriver-win64\\chromedriver.exe
+orangeHRMUrl=https://opensource-demo.orangehrmlive.com/web/index.php/auth/login
+usernameField=//input[@name='username']
+passwordField=//input[@name='password']
+loginButton=//button[@type='submit']
+username=Admin
+password=admin123
+pimmodule="//span[text()='PIM']")).click();
+configuration= "//span[text()='Configuration ']")).click();
+opitional fields= linkText("Optional Fields"));
+customfields= linkText("Custom Fields"));
+delete button= "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div[3]/div/div/div/div/div/div[1]/div[2]/div/div/button[1]/i")).click();
+Edit Button= //*[@id="app"]/div[1]/div[2]/div[2]/div/div/div[3]/div/div/div/div/div/div[1]/div[2]/div/div/button[2]/i
+Data import=//*[@id="app"]/div[1]/div[1]/header/div[2]/nav/ul/li[1]/ul/li[3]/a
+Browse a file=//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div/div/div[2]/div/div[2]
+upload=//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button
+Reportingmethods=//*[@id="app"]/div[1]/div[1]/header/div[2]/nav/ul/li[1]/ul/li[4]/a
+Add button=//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div[1]/div/button
+Adding Reporting method=//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input
+cancel=//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[1]
+save=//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]
+Termination reasons=//*[@id="app"]/div[1]/div[1]/header/div[2]/nav/ul/li[1]/ul/li[5]/a
+Adding Termintion reasons=//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div[1]/div/button
